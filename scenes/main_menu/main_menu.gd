2dd6a879042302d45a822d8b4816d4e853c7ded4
Antimundo <pablo.rubio.ru@gmail.com>
2023-11-18 08:40:10 +0100

    Add main menu

---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node
+## Used in main menu to load the game
+
+@onready var load_scene = load("res://scenes/gameplay/level_0.tscn")
+
+func _on_button_pressed():
+	var this_scene = load_scene.instantiate()
+	get_tree().root.add_child(this_scene)
+	queue_free()
