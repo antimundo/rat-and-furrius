f4c54777bade8464c0d50fe9ed6c510484224bb3
Juan Antonio Nepormoseno Rosales <nepormoseno1992@gmail.com>
2023-11-19 19:44:13 +0100

    Cat add dying sound

--- scenes/gameplay/enemies/patrol_cat.gd
+++ scenes/gameplay/enemies/patrol_cat.gd
@@ -8,6 +8,7 @@ signal player_caught
 enum Weakness { red, green, blue }
 
 @export var speed:float = 100.
+var enabled: bool = true
 
 @export var weakness: Weakness = Weakness.red:
 	set(value):
@@ -20,11 +21,21 @@ func connect_player_caught(level_manager):
 		player_caught.connect(level_manager._on_player_caught)
 
 func _physics_process(delta: float) -> void:
-	$PathFollow2D.progress += speed * delta
+	if enabled:
+		$PathFollow2D.progress += speed * delta
 
 func _on_player_entered_enemy_vision() -> void:
 	player_caught.emit()
 
 func _on_player_entered_enemy_weak_spot() -> void:
+	enabled = false
+	$PathFollow2D/Vision/Area2D.monitoring = false
+	%WeakSpot.monitoring = false
+	TEMPORAL_play_killed_sound()
 	queue_free()
-
+	
+func TEMPORAL_play_killed_sound():
+	var audio = %AudioStreamPlayer
+	audio.play()
+	audio.reparent(get_tree().root)
+	audio.finished.connect(audio.queue_free)
