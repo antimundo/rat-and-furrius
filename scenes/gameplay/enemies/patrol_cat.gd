d248d2fc8896b9581324d139e2c684fdee80e8f8
Antimundo <pablo.rubio.ru@gmail.com>
2023-11-19 22:40:35 +0100

    Add sleepy cat pulse parameters

--- scenes/gameplay/enemies/patrol_cat.gd
+++ scenes/gameplay/enemies/patrol_cat.gd
@@ -8,11 +8,24 @@ enum Weakness { all, red, green, blue }
 
 @export var speed:float = 100.
 @export var weakness: Weakness = Weakness.red
+
+@export_group("Pulse")
+## Maximum size of the pulse
+@export var size_max := 3.0
+## Time that the pulse stays off
+@export var time_off := 1.0
+## Time in the animation transition
+@export var time_animation := 1.0
+## Time trhat the pulse stays on
+@export var time_on := 1.0
+
 var enabled: bool = true
 
 func _ready() -> void:
 	%Sprite2D.set_sprite(weakness)
 	%WeakSpot.weakness = weakness
+	$PathFollow2D/Vision.scale = Vector2(0, 0)
+	animate_pulse()
 
 func connect_player_caught(level_manager):
 	if level_manager.has_method("_on_player_caught"):
@@ -31,7 +44,19 @@ func _on_player_entered_enemy_weak_spot() -> void:
 	%WeakSpot.set_deferred("monitoring", false)
 	TEMPORAL_play_killed_sound()
 	queue_free()
-	
+
+func animate_pulse():
+	var tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	var vision = $PathFollow2D/Vision
+	tween.tween_property(vision, "scale", Vector2(size_max, size_max), time_animation)
+	await tween.finished
+	await get_tree().create_timer(time_on).timeout
+	tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(vision, "scale", Vector2(0, 0), time_animation)
+	await tween.finished
+	await get_tree().create_timer(time_off).timeout
+	animate_pulse()
+
 func TEMPORAL_play_killed_sound():
 	var audio = %AudioStreamPlayer
 	audio.play()
