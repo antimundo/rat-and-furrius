7e1075c517e89b7e10b79ad5ebe208f8240f0b0e
Antimundo <pablo.rubio.ru@gmail.com>
2023-11-19 13:17:33 +0100

    Update timer UI

--- scenes/game_manager/end_game_timer.gd
+++ scenes/game_manager/end_game_timer.gd
@@ -2,14 +2,14 @@ extends Timer
 ## Used in game manager to update UI showing the time left before the game ends
 
 @onready var timer_label := %TimerValue
-@onready var timer_progress := %TimerProgressBar
+@onready var texture_progress_bar = %TimerProgressBar
 
 func _ready() -> void:
 	EventBus.got_cheese.connect(add_time)
 
 func _process(delta: float) -> void:
-	timer_label.text = "%3.2f" % time_left
-	timer_progress.ratio = time_left / wait_time
+	timer_label.text = "%3.2f s" % time_left
+	texture_progress_bar.value = time_left
 
 func add_time(time_to_add: float) -> void:
 	start(time_left + time_to_add)
