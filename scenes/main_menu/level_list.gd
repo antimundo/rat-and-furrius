3be9901b013e31202fcd1d328b0fc2ae79fdafcc
Juan Antonio Nepormoseno Rosales <nepormoseno1992@gmail.com>
2023-12-08 21:53:01 +0100

    Make end menu unique
    Fix the "ñapa" for the jam. The end menu had a child
    start menu and we passed parameters to configure its
    text and behavior when it was teh end menu.
    
    Now both scenes hardcode their text, the music used
    and the behavior.
    
    Also turned the level array into a resource stored in
    a file, so it isn't duplicated.

---
+++ scenes/main_menu/level_list.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name LevelList
+
+@export var levels: Array[PackedScene]
--- scenes/main_menu/start_menu.gd
+++ scenes/main_menu/start_menu.gd
@@ -1,24 +1,15 @@
 extends Node2D
 
-@export var scenes: Array[PackedScene]
+@export var level_list: LevelList 
 @export var boxes_to_destroy_for_game_start := 3
-@export var game_finished := false
-@export_multiline var text: String = "DESTRUYE LAS CAJAS PARA EMPEZAR"
 @onready var game:PackedScene = load("res://scenes/game_manager/game_manager.tscn")
 
 func _ready() -> void:
-	$Level/Label.text = text
-	if game_finished:
-		$AudioStreamPlayer.stream = load("res://assets/music/victory.ogg")
-		$Level/Logo.scale = Vector2(0.7, 0.7)
-		$Level/Logo.position.y -= 40
-	else:
-		$AudioStreamPlayer.stream = load("res://assets/music/music_menu.ogg")
 	$AudioStreamPlayer.play()
 
 func start_game() -> void:
 	var game_manager = game.instantiate()
-	game_manager.levels = scenes
+	game_manager.levels = level_list.levels
 	get_tree().root.add_child(game_manager)
 	queue_free()
 
